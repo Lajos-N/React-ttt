5aa7295cb7e1b9c31cdb3f5d8aabe6c4b409aa76
Lajos Nagy <lajos.j.nagy@gmail.com>
2023-05-11 11:38:17 +0200

    added board

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Row from '../Row/Row'
 
 export default function Board() {
+  const board = [
+    ['X', null, null],
+    ['O', 'O', 'X'],
+    ['X', null, null],
+  ];
+
   return (
     <>
     <div>
