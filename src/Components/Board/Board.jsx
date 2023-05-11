abcf83badeca342e505c362c145facbbe67d515d
Lajos Nagy <lajos.j.nagy@gmail.com>
2023-05-11 11:34:13 +0200

    component creation

---
+++ src/Components/Board/Board.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Row from '../Row/Row'
+
+export default function Board() {
+  return (
+    <>
+    <div>
+      <Row />
+    </div>
+    <div>
+      <Row />
+    </div>
+    <div>
+      <Row />
+    </div>
+    </>
+  )
+}
